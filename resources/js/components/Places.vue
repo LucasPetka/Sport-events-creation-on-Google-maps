5c0a7d8a51eb610643436fd98bd97d08ea5758f6
LucasPetka <lucas.petka@gmail.com>
2020-04-03 20:54:00 +0300

    Info window update

--- resources/js/components/Places.vue
+++ resources/js/components/Places.vue
@@ -73,7 +73,7 @@
 
         <!-- Modal -->
     <div class="modal fade" id="infoModal" tabindex="-1" role="dialog" aria-labelledby="infoModallLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
         <div class="modal-header">
             <h5 class="modal-title" id="infoModalLabel"><i class="fas fa-info fa-lg m-1"></i></h5>
